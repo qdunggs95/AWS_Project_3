79b74dfa141c7dfa26fbe1bbb848b0f171235edd
qdunggs95 <54254434+qdunggs95@users.noreply.github.com>
2023-08-28 09:41:57 +0700

    Update employees.ts

--- frontend/src/app/containers/Employee/actions/employees.ts
+++ frontend/src/app/containers/Employee/actions/employees.ts
@@ -95,16 +95,11 @@ export namespace EmployeeActions {
   export const addEmployee = (payload: EmployeeModel) => {
     return async (dispatch: Dispatch) => {
       dispatch(addEmployeeRequest());
-      try {
         const employeeService = new EmployeesService();
         const response = await employeeService.createEmployee(payload);
         dispatch(addEmployeeSuccess(response));
         dispatch(push('/employees'));
         showSuccessNotification(text.CHANGES_SAVED);
-      } catch (error) {
-        console.error(error);
-        showErrorNotification(text.GENERIC_ERROR);
-        dispatch(addEmployeeFailure(error));
       }
     };
   };
